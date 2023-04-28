36a1d3da53a7f67e1d6b7e3da7310e05f814707c
MarvelJo <mjmarvelm@gmail.com>
2023-04-28 22:56:43 +0700

    modified:   Assets/BG/aboutUs.png 	new file:   Assets/Icon/questionMarkDark.png 	modified:   HelpScreen/main.css 	modified:   HelpScreen/main.html 	modified:   HelpScreen/main.js

--- HelpScreen/main.js
+++ HelpScreen/main.js
@@ -23,18 +23,91 @@ exp.addEventListener("mouseleave", resetPos);
 
 const search = document.querySelector(".search");
 const askUs = document.querySelector(".row-line h1");
+const imgQues = document.querySelector(".questionMark img");
+const submitbtn = document.querySelector(".search_submit");
+const inputan = document.getElementById("idsearch");
+var clicked = false;
 
 search.addEventListener("mouseover", () => {
-  search.style.color = "#d48c00";
-  askUs.style.transform = "scale(0.5)";
-  askUs.style.opacity = "0";
   askUs.style.color = "#d48c00";
-  //   rowLine.style.transform = "rotate(90deg)";
+  imgQues.src = "../Assets/Icon/questionMarkDark.png";
 });
 
-search.addEventListener("mouseleave", () => {
-  askUs.style.transform = "scale(1)";
-  askUs.style.opacity = "1";
+search.addEventListener("mouseout", () => {
   askUs.style.color = "#f9a43d";
-  //   rowLine.style.transform = "rotate(0deg)";
+  imgQues.src = "../Assets/Icon/questionMark.png";
+});
+
+search.addEventListener("focus", () => {
+  search.classList.add("extended");
+  imgQues.style.transform = "rotate(-24deg)";
+  askUs.style.transform = "scale(0.5)";
+  askUs.style.opacity = "0";
+  clicked = true;
+});
+
+// search.addEventListener("blur", () => {
+// });
+
+search.addEventListener("focusout", () => {
+  search.classList.remove("extended");
+  const textInput = inputan.value;
+  if (textInput.length != 0) {
+    var textItal = "PROCESSING YOUR QUESTION...";
+    askUs.innerHTML = textItal.italics();
+    search.value = "";
+    imgQues.style.transform = "rotate(0deg)";
+    askUs.style.transform = "scale(1)";
+    askUs.style.opacity = "1";
+    setTimeout(() => {
+      askUs.style.transform = "scale(0.9)";
+      askUs.style.opacity = "0";
+    }, 2500);
+    setTimeout(() => {
+      var textItal = "ASK US ANY QUESTIONS";
+      askUs.innerHTML = textItal.italics();
+      askUs.style.transform = "scale(1)";
+      askUs.style.opacity = "1";
+    }, 3000);
+  } else {
+    imgQues.style.transform = "rotate(0deg)";
+    askUs.style.transform = "scale(1)";
+    askUs.style.opacity = "1";
+  }
+  clicked = false;
+});
+
+document.addEventListener("keypress", function (e) {
+  if (clicked) {
+    var keynum = e.keyCode || e.which;
+    if (keynum == 13) {
+      search.classList.remove("extended");
+      const textInput = inputan.value;
+      if (textInput.length != 0) {
+        var textItal = "PROCESSING YOUR QUESTION...";
+        askUs.innerHTML = textItal.italics();
+        search.value = "";
+		search.blur();
+        imgQues.style.transform = "rotate(0deg)";
+        askUs.style.transform = "scale(1)";
+        askUs.style.opacity = "1";
+        setTimeout(() => {
+          askUs.style.transform = "scale(0.9)";
+          askUs.style.opacity = "0";
+        }, 2500);
+        setTimeout(() => {
+          var textItal = "ASK US ANY QUESTIONS";
+          askUs.innerHTML = textItal.italics();
+          askUs.style.transform = "scale(1)";
+          askUs.style.opacity = "1";
+        }, 3000);
+      } else {
+		search.blur();
+        imgQues.style.transform = "rotate(0deg)";
+        askUs.style.transform = "scale(1)";
+        askUs.style.opacity = "1";
+      }
+      clicked = false;
+    }
+  }
 });
